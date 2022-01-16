7d0a0c3a6b0e69609ddd4971750cd879c04a9650
PolPoteu <ratattuja@gmail.com>
2022-01-17 00:02:29 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { PostModule } from './post/post.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PostModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PostModule,
+    HttpClientModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
