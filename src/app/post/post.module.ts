abf9cd4d2644f4470ee93e7afa09f7e5322b77ae
PolPoteu <ratattuja@gmail.com>
2022-01-16 15:39:16 +0100

    feat: add routing to post

---
+++ src/app/post/post.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PostRoutingModule } from './post-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PostRoutingModule],
+})
+export class PostModule {}
